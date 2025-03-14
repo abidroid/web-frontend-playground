5d1f2affa7e7f1ae68f1075e83784e59f3852ade
Muhammad Abid <abidroid1@gmail.com>
2025-03-14 22:05:28 +0500

    oop1

--- js/oop1.js
+++ js/oop1.js
@@ -5,7 +5,8 @@ Prototypes in JavaScript
 Task: Prototype Chaining
 Create a constructor function Animal that has a method speak() that return 'Animal speaking'.
 Then create another constructor Dog that inherits from Animal using prototypes.
-The Dog constructor should add a method bark() that returns 'Woof!'. Demonstrate the prototype chain between Dog and Animal.
+The Dog constructor should add a method bark() that returns 'Woof!'. 
+Demonstrate the prototype chain between Dog and Animal.
 Learning objective
  
  
@@ -16,20 +17,23 @@ function Dog() {
 }
 */
 
-function Animal(){
+function Animal(){}
 
-     this.speak = function(){
-        console.log('Animal speaking');
-    }
+Animal.prototype.speak = function(){
+    return 'Animal speaking';
 }
 
-function Dog(){
-    this.bark = function(){
-        console.log('Woof!');
-    }
+function Dog(){}
+
+Dog.prototype = Object.create(Animal.prototype);
+
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function(){
+    return 'Woof!';
 }
 
-const animal = new Animal();
-animal.speak();
 const dog = new Dog();
-dog.bark();
+
+console.log(dog.speak());
+console.log(dog.bark());
