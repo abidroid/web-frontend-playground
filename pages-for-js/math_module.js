1182f5484556c6b1cf369e52652d679199e85323
Muhammad Abid <abidroid1@gmail.com>
2025-03-30 08:28:19 +0500

    math  module

---
+++ pages-for-js/math_module.js
@@ -0,0 +1,11 @@
+function doubltIt(value){
+    console.log(`Double of ${value} is ${2*value}`);
+    
+}
+
+function multiplyBy5(value){
+    console.log(`${value} multiplied by 5 = ${5*value}`);
+    
+}
+
+export { doubltIt, multiplyBy5};
