60cc76a901683e474c27f63a67a84600d0fc55c3
Muhammad Abid <abidroid1@gmail.com>
2025-04-27 21:12:04 +0500

    done

--- js/array-example.js
+++ js/array-example.js
@@ -1,7 +1,22 @@
 
 function removeDuplicates(arr) {
-    console.log('yes');
-    
-}
 
-removeDuplicates();
+    const uniqueSet = new Set();
+    const result = [];
+
+    for( const num of arr ){
+
+        if( !uniqueSet.has(num)){
+
+            uniqueSet.add(num);
+            result.push(num);
+        }
+    }
+
+    return result;
+}
+const inputArray = [1, 2, 3, 2, 1, 4, 5, 3, 2, 2, 4, 5];
+console.log(inputArray);
+
+const outputArray = removeDuplicates(inputArray);
+console.log(outputArray);
